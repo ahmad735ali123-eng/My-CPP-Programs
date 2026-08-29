6f65ff093663ccedfa34e51653459e8cdbcfdcb5
Ahmad Ali <ahmad735ali123@gmail.com>
2026-08-29 17:01:02 +0500

    Add files via upload

---
+++ Sum_of_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int sum;
+    int a;
+    int b;
+    cout<<"Enter First Number:";
+    cin>>a;
+    cout<<"Enter Second Number:";
+    cin>>b;
+    sum = a+b;    
+    cout<<"Sum of two Numbers is:"<<sum<<endl;
+    return 0;
+}
